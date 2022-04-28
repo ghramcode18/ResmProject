3d531216f2ca0a70146af48183d70fe006fbc646
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2022-04-28 12:00:05 +0300

    initial

---
+++ src/main/java/The/Geeks/ResmProject/ResmProjectApplication.java
@@ -0,0 +1,13 @@
+package The.Geeks.ResmProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/The/Geeks/ResmProject/ResmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package The.Geeks.ResmProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
