9a2563dff3138b835143321de388c5133508564d
Hiba-mousli <hibamousli85@gmil.com>
2022-05-24 14:10:53 +0300

    --

--- src/main/java/The/Geeks/ResmProject/service/UserService.java
+++ src/main/java/The/Geeks/ResmProject/service/UserService.java
@@ -1,6 +1,10 @@
 package The.Geeks.ResmProject.service;
 
+<<<<<<< Updated upstream
 import The.Geeks.ResmProject.model.UserModel;
+=======
+import The.Geeks.ResmProject.domain.User;
+>>>>>>> Stashed changes
 
 public interface UserService {
     void singUp(UserModel user) throws Exception;
--- src/main/java/The/Geeks/ResmProject/service/UserServiceImpl.java
+++ src/main/java/The/Geeks/ResmProject/service/UserServiceImpl.java
@@ -1,8 +1,14 @@
 package The.Geeks.ResmProject.service;
+<<<<<<< Updated upstream
+=======
+
+import java.util.regex.Pattern;
+>>>>>>> Stashed changes
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+<<<<<<< Updated upstream
 import The.Geeks.ResmProject.domain.Address;
 import The.Geeks.ResmProject.domain.City;
 import The.Geeks.ResmProject.domain.Country;
@@ -14,11 +20,15 @@ import The.Geeks.ResmProject.repo.AddressRepo;
 import The.Geeks.ResmProject.repo.CityRepo;
 import The.Geeks.ResmProject.repo.CountryRepo;
 import The.Geeks.ResmProject.repo.RegionRepo;
+=======
+import The.Geeks.ResmProject.domain.User;
+>>>>>>> Stashed changes
 import The.Geeks.ResmProject.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 
 @Service @RequiredArgsConstructor @Component
 public class UserServiceImpl implements UserService {
+<<<<<<< Updated upstream
 
     private final UserRepo userRepo;
     private final AddressRepo addressRepo;
@@ -29,6 +39,35 @@ public class UserServiceImpl implements UserService {
     @Override
     public void singUp(UserModel user) throws Exception
     {
+=======
+    private final UserRepo userRepo;
+    @Override
+    public void singUp(User user) throws Exception {
+        if(checkIfUserExist(user.getUsername()))
+        {
+            throw new Exception("User already exists for this email ");
+        }
+        else if(! isValid(user.getUsername()))
+        {
+            throw new Exception("Sorry, this email is not valid");
+        }
+        else
+        {
+            User Euser = new User();
+            Euser.setUsername(user.getUsername());
+            Euser.setHashedPassword(user.getHashedPassword());
+            Euser.setFirstName(user.getFirstName());
+            Euser.setLastName(user.getLastName());
+            Euser.getAddress().setLattitude(user.getAddress().getLattitude());
+            Euser.getAddress().setLongitutde(user.getAddress().getLongitutde());
+            Euser.getAddress().getRegion().setName(user.getAddress().getRegion().getName());
+            Euser.getAddress().getRegion().getCity().setName(user.getAddress().getRegion().getCity().getName());
+            Euser.getAddress().getRegion().getCity().getCountry().setName(user.getAddress().getRegion().getCity().getCountry().getName());
+            
+        }
+
+    }
+>>>>>>> Stashed changes
 
         if(checkIfUserExist(user.getUsername()))
         {
@@ -64,5 +103,23 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean checkIfUserExist(String username) {
         return userRepo.findByUsername(username) !=null ? true : false;
+<<<<<<< Updated upstream
     }
 }
+=======
+    }
+    public static boolean isValid(String username)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (username == null)
+            return false;
+        return pat.matcher(username).matches();
+    }
+
+    
+}
+>>>>>>> Stashed changes
